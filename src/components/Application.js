8044ded67a9e8c497a23f46b34a5007f83285b4d
Chris Bowyer <100442597+Devcab2@users.noreply.github.com>
2022-10-07 11:55:40 -0600

    import axios + useEffect, make first get request, remove hardcoded days data and update application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,28 +1,60 @@
+import React from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
-import {useState} from "react";
-import React from "react";
+import {useState, useEffect} from "react";
+import Appointment from "./Appointment";
+import axios from "axios";
 
-const days = [
+const appointments = [
   {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
   {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
   },
   {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      },
+    },
+  },
+  {
+    id: 5,
+    time: "4pm",
   },
 ];
 
 export default function Application(props) {
+  const [days,setDays] = useState([]);
   const [day, setDay] = useState("Monday");
+
+  useEffect(() => {
+    axios.get("/api/days")
+    .then((response) => {
+      setDays(response.data)
+    })
+  }, []);
+  
   return (
     <main className="layout">
       <section className="sidebar">
@@ -33,7 +65,11 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList 
+          days={days} 
+          value={day} 
+          onChange={setDay} 
+          />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -42,7 +78,16 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+      {appointments.map((appointment) => (
+          <Appointment 
+          key={appointment.id} 
+          {...appointment} 
+          />
+        ))}
+          <Appointment 
+          key="last" 
+          time="5pm" 
+          />
       </section>
     </main>
   );
